e05e3b4de9bd2b74ae676182a2ec2749e2e98c67
Paweł Kapalla <pkapalla@xesenix.pl>
2013-10-17 05:38:42 +0200

    maven setup

---
+++ src/main/java/timeline/pl/xesenix/timeline/App.java
@@ -0,0 +1,14 @@
+
+package timeline.pl.xesenix.timeline;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
